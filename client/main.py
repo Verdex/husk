5ca2fe9588e6be093136c384c47f8010c77835ca
David L. Inman <davidlevere@gmail.com>
2021-04-05 23:45:20 -0400

    main.py skeleton

---
+++ client/main.py
@@ -0,0 +1,50 @@
+
+import pygame
+
+EventLoopWait = 16
+DefaultHeight = 500
+DefaultWidth = 800
+White = [255, 255, 255]
+
+pygame.init()
+
+best_depth = pygame.display.mode_ok((DefaultWidth, DefaultHeight), 0, 32)
+screen = pygame.display.set_mode((DefaultWidth, DefaultHeight), pygame.RESIZABLE, best_depth) 
+
+loop_start = 0
+
+# TODO 
+active = True
+event_loop_delta = 0
+
+while active:
+    ticks = pygame.time.get_ticks()
+    event_loop_delta = ticks - loop_start
+    loop_start = ticks
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            pass
+        elif event.type == pygame.KEYUP:
+            pass
+        elif event.type == pygame.MOUSEBUTTONDOWN: 
+            pass
+        elif event.type == pygame.MOUSEBUTTONUP: 
+            pass
+        elif event.type == pygame.VIDEORESIZE:
+            pass
+        elif event.type == pygame.QUIT:
+            pass
+
+    screen.fill(White)
+
+
+    pygame.display.update()
+
+    loop_end = pygame.time.get_ticks()
+
+    loop_duration = loop_end - loop_start
+
+    if loop_duration < EventLoopWait:
+        pygame.time.delay(EventLoopWait - loop_duration)
+    
