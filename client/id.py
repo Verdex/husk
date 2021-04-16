3676a0c31e2d4ec26db7b50b5052f318a96fa1c0
David L. Inman <davidlevere@gmail.com>
2021-04-15 22:56:52 -0400

    id manager

--- client/id.py
+++ client/id.py
@@ -1,11 +1,18 @@
 
+class LocalIdManager:
+
+    def __init__(self):
+        self.current = 1
+
+    def new(self):
+        id = LocalId(self.current)
+        self.current += 1
+        return id
+
 class LocalId:
 
     def __init__(self, value):
         self.value = value
 
-    def new(self):
-        id = self.current 
-        self.current += 1
 
 
