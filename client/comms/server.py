431ac690b19573dc5d567614906b1217db354240
David L. Inman <davidlevere@gmail.com>
2021-05-16 17:42:51 -0400

    server stuff

--- client/comms/server.py
+++ client/comms/server.py
@@ -1,10 +1,30 @@
 
+import struct
 import socket
+from functools import reduce
+
+class Requests:
+    def __init__(self):
+        self.rs = []
+
+    def add(self, request):
+        self.rs.append(request)
+
+    def output(self):
+        x = self.rs
+        self.rs = []
+        return x
 
 class Server:
-    pass
+
+    def __init__(self):
+        pass
+
+    def send_requests():
+        rs = OutGoingRequests.output()
 
 
 
 
+OutGoingRequests = Requests()
 GameServer = Server()
