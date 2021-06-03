9b501ec7551d96a8a0019258ed8344bffac232c3
David L. Inman <davidlevere@gmail.com>
2021-06-02 22:41:15 -0400

    more done in process

--- server/src/processing/process.rs
+++ server/src/processing/process.rs
@@ -3,18 +3,21 @@ use std::net::TcpStream;
 use std::sync::mpsc::{Sender, Receiver, SendError};
 use std::thread::{self, JoinHandle};
 
-use crate::comms::stream::{read_requests};
+use crate::comms::stream;
+use crate::comms::data::UserRequests;
 use crate::data::ProcComm;
+use crate::game_engine::interface::Action;
 
 
-pub fn start(receiver : Receiver<ProcComm<TcpStream>>) -> JoinHandle<()> {
+pub fn start( receiver : Receiver<ProcComm<TcpStream>>
+            , game_engine_sender : Sender<ProcComm<Action>> ) -> JoinHandle<()> {
 
     let handle = thread::spawn(move || {
         loop {
             let msg = receiver.recv().expect("Receive Failure in Process");
 
             match msg {
-                ProcComm::Message(stream) => process_stream(stream),
+                ProcComm::Message(stream) => process_stream(stream, &game_engine_sender),
                 ProcComm::Stop => break,
             }
         }
@@ -23,13 +26,19 @@ pub fn start(receiver : Receiver<ProcComm<TcpStream>>) -> JoinHandle<()> {
     handle
 }
 
-fn process_stream(mut stream : TcpStream) {
-    let user_requests_result = read_requests(&mut stream);
+fn process_stream( mut stream : TcpStream
+                 , game_engine_sender : &Sender<ProcComm<Action>> ) {
+
+    let user_requests_result = stream::read_requests(&mut stream);
 
     match user_requests_result {
-        Ok(user_requests) => println!("USER REQUESTS"),
+        Ok(user_requests) => process_requests(user_requests, game_engine_sender),
         Err(e) => println!("Encountered error while parsing TCP Stream: {}", e),
     }
+}
+
+fn process_requests( user_requests : UserRequests, game_engine_sender : &Sender<ProcComm<Action>> ) {
+    // TODO 
 }
 
 pub fn stop(process_sender : Sender<ProcComm<TcpStream>> ) {
